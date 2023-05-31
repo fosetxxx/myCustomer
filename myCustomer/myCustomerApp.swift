81d679aa44252cad3b5bab784ac377d8ef2f122b
Semih Karahan <karahansemih@hotmail.com>
2023-05-31 21:57:39 +0300

    Initial Commit

---
+++ myCustomer/myCustomerApp.swift
@@ -0,0 +1,17 @@
+//
+//  myCustomerApp.swift
+//  myCustomer
+//
+//  Created by Semih Karahan on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct myCustomerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
